53264ce201b472b8cdce5ee3081188d99cde80ff
EmaSantoro <emsantoro@frba.utn.edu.ar>
2025-04-29 14:12:51 -0300

    ahora si

--- src/main/java/ar/edu/utn/frba/dds/solicitud/SolicitudDeEliminacion.java
+++ src/main/java/ar/edu/utn/frba/dds/solicitud/SolicitudDeEliminacion.java
@@ -9,19 +9,20 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
-
 public class SolicitudDeEliminacion {
     private Hecho hecho;
     private String descripcion;
     private EstadoDeSolicitud estadoDeSolicitud;
     private LocalDateTime fechaDeCreacion;
     private LocalDateTime fechaDeEvaluacion;
+    @Setter
     private Administrador administradorQueEvaluo;
+
+    private static Integer caracteresMinimos = 500;
 
     public SolicitudDeEliminacion(Hecho hecho, String descripcion) {
         this.hecho = hecho;
-        this.descripcion = descripcion;
+        setDescripcion(descripcion);
         this.estadoDeSolicitud = EstadoDeSolicitud.PENDIENTE;
         this.fechaDeCreacion = LocalDateTime.now();
     }
@@ -38,8 +39,10 @@ public class SolicitudDeEliminacion {
         estadoDeSolicitud = EstadoDeSolicitud.RECHAZADA;
     }
 
-    public SolicitudDeEliminacion crearSolicitud(Hecho hecho, String descripcion) {
-        ValidadorSolicitud.validarSolicitud(descripcion);
-        return new SolicitudDeEliminacion(hecho, descripcion);
+    public void setDescripcion(String descripcion) {
+        if (descripcion == null || descripcion.length() < caracteresMinimos) {
+            throw new IllegalArgumentException("La descripción debe tener minimo " + caracteresMinimos );
+        }
+        this.descripcion = descripcion;
     }
 }
