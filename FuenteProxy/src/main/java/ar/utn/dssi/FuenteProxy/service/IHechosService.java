6fbac56d4d3fe0c800c4582e4ecac36f2b5c962c
vramma_ <ramivera2003@gmail.com>
2025-05-24 23:28:50 -0300

    servicio de hechos completo, implementacion de consumo de adapter

--- FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/service/IHechosService.java
+++ FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/service/IHechosService.java
@@ -1,4 +1,10 @@
 package ar.utn.dssi.FuenteProxy.service;
 
+import ar.utn.dssi.FuenteProxy.models.DTOs.output.HechoOutputDTO;
+import ar.utn.dssi.FuenteProxy.models.entities.Hecho;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
 public interface IHechosService {
+  public List<HechoOutputDTO> obtenerHechos();
 }
--- FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/service/impl/HechosService.java
+++ FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/service/impl/HechosService.java
@@ -1,4 +1,33 @@
 package ar.utn.dssi.FuenteProxy.service.impl;
 
-public class HechosService {
+import ar.utn.dssi.FuenteProxy.models.DTOs.output.HechoOutputDTO;
+import ar.utn.dssi.FuenteProxy.models.adapter.IServicioExternoAdapter;
+import ar.utn.dssi.FuenteProxy.models.adapter.adaptadoresConcretos.DesastresNaturalesAdapter;
+import ar.utn.dssi.FuenteProxy.models.entities.Hecho;
+import ar.utn.dssi.FuenteProxy.service.IHechosService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class HechosService implements IHechosService {
+  public IServicioExternoAdapter desastreNaturalesAdapter;
+
+  public HechosService() {
+    this.desastreNaturalesAdapter = new DesastresNaturalesAdapter();
+  }
+
+  public List<HechoOutputDTO> obtenerHechos() {
+    return desastreNaturalesAdapter.obtenerHechos();
+  }
+
+  private HechoOutputDTO hechoOutputDTO(Hecho hecho) {
+    var outputDTO = new HechoOutputDTO();
+    outputDTO.setTitulo(hecho.getTitulo());
+    outputDTO.setDescripcion(hecho.getDescripcion());
+    outputDTO.setCategoria(hecho.getCategoria());
+    outputDTO.setUbicacion(hecho.getUbicacion());
+    outputDTO.setFechaAcontecimiento(hecho.getFechaAcontecimiento());
+    outputDTO.setFechaCarga(hecho.getFechaCarga());
+    return outputDTO;
+  }
 }
