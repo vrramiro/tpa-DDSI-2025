644adad3e287a4e15cc4ccb7c8a9b41d29763ed7
vramma_ <ramivera2003@gmail.com>
2025-05-26 19:50:47 -0300

    obtencion de hechos de la api desastres naturales

--- FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/models/adapter/adaptados/DesastresNaturales.java
+++ FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/models/adapter/adaptados/DesastresNaturales.java
@@ -1,7 +1,10 @@
 package ar.utn.dssi.FuenteProxy.models.adapter.adaptados;
 
+import ar.utn.dssi.FuenteProxy.models.DTOs.external.DesastresNaturales.HechoDesastresNaturales;
+import ar.utn.dssi.FuenteProxy.models.DTOs.external.DesastresNaturales.HechosDesastresNaturales;
 import ar.utn.dssi.FuenteProxy.models.DTOs.output.HechoOutputDTO;
 import org.springframework.web.reactive.function.client.WebClient;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DesastresNaturales {
@@ -13,7 +16,36 @@ public class DesastresNaturales {
   }
 
   //TODO
-  private List<HechoOutputDTO> obtenerHechosDeAPI() {
-    return List.of();
+  public List<HechoDesastresNaturales> obtenerHechosDeAPI() {
+    Integer pagina = 1;
+    List<HechoDesastresNaturales> hechosObtenidos = new ArrayList<HechoDesastresNaturales>();
+
+    while(true) {
+        HechosDesastresNaturales hechosPedidos = hechosPorPagina(pagina);
+
+        //Cuando pedimos hechos a una pagina que no tiene hechos la API devuelve una lista vacia
+        if(hechosPedidos.getHechosObtenidos().isEmpty()) break;
+
+        hechosObtenidos.addAll(hechosPedidos.getHechosObtenidos());
+
+        if(hechosPedidos.getCurrent_page() >= hechosPedidos.getLast_page()) break;
+
+        pagina++;
+    }
+
+    return hechosObtenidos;
+  }
+
+  private HechosDesastresNaturales hechosPorPagina(Integer pagina) {
+    return webClient
+        .get()
+        .uri(uriBuilder -> uriBuilder
+            .path("/api/desastres")
+            .queryParam("page", pagina)
+            .queryParam("per_page", 100)
+            .build())
+        .retrieve()
+        .bodyToMono(HechosDesastresNaturales.class)
+        .block();
   }
 }
