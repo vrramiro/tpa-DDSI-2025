68aab129cfe6956cff26a2a7e57f62d05eb313c8
vramma_ <ramivera2003@gmail.com>
2025-05-26 19:48:05 -0300

    agrego obtencion de hechos de instancia metamapa

--- FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/service/IHechosService.java
+++ FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/service/IHechosService.java
@@ -1,10 +1,9 @@
 package ar.utn.dssi.FuenteProxy.service;
 
 import ar.utn.dssi.FuenteProxy.models.DTOs.output.HechoOutputDTO;
-import ar.utn.dssi.FuenteProxy.models.entities.Hecho;
-import org.springframework.stereotype.Service;
 import java.util.List;
 
 public interface IHechosService {
-  public List<HechoOutputDTO> obtenerHechos();
+  List<HechoOutputDTO> obtenerHechos();
+  List<HechoOutputDTO> obtenerHechosInstanciasMetamapa();
 }
