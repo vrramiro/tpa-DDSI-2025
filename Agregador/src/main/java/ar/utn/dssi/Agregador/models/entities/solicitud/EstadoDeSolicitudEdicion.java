55956a20fdde79f567a8204bf5ed8e62601fa5f3
EmaSantoro <165762742+EmaSantoro@users.noreply.github.com>
2025-05-26 23:06:04 -0300

    edicion solicitud

---
+++ Agregador/src/main/java/ar/utn/dssi/Agregador/models/entities/solicitud/EstadoDeSolicitudEdicion.java
@@ -0,0 +1,8 @@
+package src.main.java.ar.utn.dssi.Agregador.models.entities.solicitud;
+
+public enum EstadoDeSolicitudEdicion {
+    PENDIENTE,
+    ACEPTADA,
+    ACEPTADA_CON_MODIFICACIONES,
+    RECHAZADA
+}
