0b0c4ec0929349bb9bbe919eec22a6a94110c386
vramma_ <ramivera2003@gmail.com>
2025-05-26 22:04:47 -0300

    agrego autenticacion, no iba a permitir obtener los hechos dado que no estaba logueado previamente

--- FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/models/adapter/adaptados/DesastresNaturales.java
+++ FuenteProxy/src/main/java/ar/utn/dssi/FuenteProxy/models/adapter/adaptados/DesastresNaturales.java
@@ -1,7 +1,9 @@
 package ar.utn.dssi.FuenteProxy.models.adapter.adaptados;
 
+import ar.utn.dssi.FuenteProxy.models.DTOs.external.DesastresNaturales.DatosLogin;
 import ar.utn.dssi.FuenteProxy.models.DTOs.external.DesastresNaturales.HechoDesastresNaturales;
 import ar.utn.dssi.FuenteProxy.models.DTOs.external.DesastresNaturales.HechosDesastresNaturales;
+import ar.utn.dssi.FuenteProxy.models.DTOs.external.DesastresNaturales.RespuestaLogin;
 import ar.utn.dssi.FuenteProxy.models.DTOs.output.HechoOutputDTO;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.util.ArrayList;
@@ -9,14 +11,32 @@ import java.util.List;
 
 public class DesastresNaturales {
   private final WebClient webClient;
+  private String token;
 
   //TODO: completar instanciacion de webClient
   public DesastresNaturales() {
     this.webClient = WebClient.builder().baseUrl("https://api-ddsi.disilab.ar/public").build();
   }
 
-  //TODO
+  private void autenticarse() {
+    //TODO por seguridad no deberia ser asi pero por simplicidad lo dejamos asi por ahora
+    //Datos irian en el properties y no deberia estar commiteado en el repo
+    DatosLogin datosLogin = new DatosLogin("ddsi@gmail.com", "ddsi2025*");
+
+    RespuestaLogin respuesta = webClient
+        .post()
+        .uri(uriBuilder -> uriBuilder.path("/auth/login").build())
+        .bodyValue(datosLogin)
+        .retrieve()
+        .bodyToMono(RespuestaLogin.class)
+        .block();
+
+    this.token = respuesta.getData().getAccess_token();
+  }
+
   public List<HechoDesastresNaturales> obtenerHechosDeAPI() {
+    this.autenticarse();
+
     Integer pagina = 1;
     List<HechoDesastresNaturales> hechosObtenidos = new ArrayList<HechoDesastresNaturales>();
 
@@ -44,6 +64,7 @@ public class DesastresNaturales {
             .queryParam("page", pagina)
             .queryParam("per_page", 100)
             .build())
+        .header("Authorization", "Bearer " + token)
         .retrieve()
         .bodyToMono(HechosDesastresNaturales.class)
         .block();
