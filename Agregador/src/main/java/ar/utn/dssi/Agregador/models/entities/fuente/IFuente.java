901170d0c2c0ca3b7d1087aa28632f7d43d00487
vramma_ <ramivera2003@gmail.com>
2025-06-17 12:41:01 -0300

    primer aproach de fuentes, tiene que cambiar

--- Agregador/src/main/java/ar/utn/dssi/Agregador/models/entities/fuente/IFuente.java
+++ Agregador/src/main/java/ar/utn/dssi/Agregador/models/entities/fuente/IFuente.java
@@ -1,4 +1,8 @@
 package ar.utn.dssi.Agregador.models.entities.fuente;
 
+import ar.utn.dssi.Agregador.models.DTOs.inputDTO.HechoInputDTO;
+import java.util.List;
+
 public interface IFuente {
+  public List<HechoInputDTO> obtenerHechos();
 }
