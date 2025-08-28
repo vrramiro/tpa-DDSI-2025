6db355506ab2c428a75f0bb93d534ea2de896082
NicolasTuma <nicolas.tuma@gmail.com>
2025-08-28 10:43:04 -0300

    borro js incesesario

--- FRONT/js/explorador.js
+++ FRONT/js/explorador.js
@@ -7,28 +7,3 @@ const mainNav = document.getElementById("main-nav");
 menuToggle.addEventListener("click", () => {
   mainNav.classList.toggle("show");
 });
-
-// ============================================================
-// FILTROS
-// ============================================================
-const btnFiltrar = document.getElementById("btn-filtrar");
-btnFiltrar.addEventListener("click", () => {
-  const desde = document.getElementById("fecha-desde").value;
-  const hasta = document.getElementById("fecha-hasta").value;
-  const origen = document.getElementById("origen").value;
-  const categoria = document.getElementById("categoria").value;
-  const ubicacion = document.getElementById("ubicacion").value;
-
-  alert(
-    `📌 Filtros seleccionados:\n\nFecha desde: ${desde}\nFecha hasta: ${hasta}\nOrigen: ${origen}\nCategoría: ${categoria}\nUbicación: ${ubicacion}`
-  );
-});
-
-// ============================================================
-// VER COLECCIONES
-// ============================================================
-const btnColecciones = document.getElementById("btn-colecciones");
-btnColecciones.addEventListener("click", () => {
-  alert("👉 Próximamente: vista de colecciones.");
-  // Ejemplo: window.location.href = "colecciones.html";
-});
