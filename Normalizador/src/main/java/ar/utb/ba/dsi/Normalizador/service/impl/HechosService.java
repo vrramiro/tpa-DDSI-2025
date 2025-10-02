2f3f86d62a75b73b72161ee04c87c24264957a53
vramma_ <ramivera2003@gmail.com>
2025-10-02 02:13:56 -0300

    arreglo proxy para importar correctamente los archivos de las fuentes

--- Normalizador/src/main/java/ar/utb/ba/dsi/Normalizador/service/impl/HechosService.java
+++ Normalizador/src/main/java/ar/utb/ba/dsi/Normalizador/service/impl/HechosService.java
@@ -1,29 +1,22 @@
 package ar.utb.ba.dsi.Normalizador.service.impl;
 
 import ar.utb.ba.dsi.Normalizador.models.DTOs.Input.HechoInputDTO;
-import ar.utb.ba.dsi.Normalizador.models.DTOs.Output.CategoriaOutputDTO;
 import ar.utb.ba.dsi.Normalizador.models.DTOs.Output.HechoOutputDTO;
-import ar.utb.ba.dsi.Normalizador.models.DTOs.Output.UbicacionOutputDTO;
 import ar.utb.ba.dsi.Normalizador.models.entities.Categoria;
 import ar.utb.ba.dsi.Normalizador.models.entities.Hecho;
 import ar.utb.ba.dsi.Normalizador.models.entities.Ubicacion;
 import ar.utb.ba.dsi.Normalizador.models.entities.errores.CategoriaNoEcontrada;
 import ar.utb.ba.dsi.Normalizador.models.entities.errores.HechoNoEcontrado;
 import ar.utb.ba.dsi.Normalizador.models.entities.sanitizador.Sanitizador;
-import ar.utb.ba.dsi.Normalizador.models.mappers.MapperDeCategorias;
 import ar.utb.ba.dsi.Normalizador.models.mappers.MapperDeFecha;
 import ar.utb.ba.dsi.Normalizador.models.mappers.MapperDeHechos;
-import ar.utb.ba.dsi.Normalizador.models.mappers.MapperDeUbicacion;
-import ar.utb.ba.dsi.Normalizador.models.repository.ICategoriaRepository;
 import ar.utb.ba.dsi.Normalizador.service.ICategoriaService;
 import ar.utb.ba.dsi.Normalizador.service.IHechosService;
 import ar.utb.ba.dsi.Normalizador.service.IUbicacionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
 
 @Service
 public class HechosService implements IHechosService {
