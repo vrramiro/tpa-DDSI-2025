2d648ecd4eef6266822a41a25219ed41bee3ee23
EmaSantoro <emsantoro@frba.utn.edu.ar>
2025-10-03 00:45:29 -0300

    unifico DTOs entre f dinamica y agregador

--- FuenteDinamica/src/main/java/ar/utn/dssi/FuenteDinamica/models/DTOs/inputs/HechoInputDTONormalizador.java
+++ FuenteDinamica/src/main/java/ar/utn/dssi/FuenteDinamica/models/DTOs/inputs/HechoInputDTONormalizador.java
@@ -12,8 +12,9 @@ import java.time.LocalDateTime;
 public class HechoInputDTONormalizador {
     private String titulo;
     private String descripcion;
+    private String tituloSanitizado;
+    private String descripcionSanitizada;
     private CategoriaInputDTO categoria;
     private UbicacionInputDTO ubicacion;
     private LocalDateTime fechaAcontecimiento;
-    private LocalDateTime fechaCarga;
 }
--- FuenteDinamica/src/main/java/ar/utn/dssi/FuenteDinamica/models/DTOs/outputs/HechoOutputDTO.java
+++ FuenteDinamica/src/main/java/ar/utn/dssi/FuenteDinamica/models/DTOs/outputs/HechoOutputDTO.java
@@ -13,13 +13,18 @@ import java.util.List;
 @Setter
 public class HechoOutputDTO {
   private Long idHechoOrigen;
+
   private String titulo;
   private String descripcion;
+
   private String tituloSanitizado;
   private String descripcionSanitizado;
+
   private CategoriaOutputDTO categoria;
   private UbicacionOutputDTO ubicacion;
+
   private LocalDateTime fechaAcontecimiento;
   private LocalDateTime fechaCarga;
+
   private List<String> contenidoMultimedia;
 }
