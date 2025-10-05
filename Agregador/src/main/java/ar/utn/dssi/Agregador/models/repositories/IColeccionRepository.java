9751cbc57f62fbe87cc6f29676cffb084e858f37
vramma_ <ramivera2003@gmail.com>
2025-10-05 02:50:12 -0300

    elimino metodos redundantes, agrego nuevos metodos

--- Agregador/src/main/java/ar/utn/dssi/Agregador/models/repositories/IColeccionRepository.java
+++ Agregador/src/main/java/ar/utn/dssi/Agregador/models/repositories/IColeccionRepository.java
@@ -2,13 +2,9 @@ package ar.utn.dssi.Agregador.models.repositories;
 
 import ar.utn.dssi.Agregador.models.entities.Coleccion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 public interface IColeccionRepository extends JpaRepository<Coleccion, String> {
-  Coleccion findByHandle(String handle);
-
-  void update(Coleccion coleccion);
-
+  Coleccion findColeccionByHandle(String handle);
+  List<Coleccion> findColeccionByActualizada(Boolean actualizada);
 }
