fc02ed196ef0316dad3fd8378b510960e01234e2
NicolasTuma <nicolas.tuma@gmail.com>
2025-10-05 20:46:24 -0300

    Arreglo cagada...

--- Frontend/src/main/resources/static/js/home/explorador.js
+++ Frontend/src/main/resources/static/js/home/explorador.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([37.8882, -4.7794], 5);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
