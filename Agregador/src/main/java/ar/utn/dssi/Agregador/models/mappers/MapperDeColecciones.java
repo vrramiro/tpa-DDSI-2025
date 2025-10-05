a81f9531e69daa15b9a0127dfe8c676e5b141dcb
vramma_ <ramivera2003@gmail.com>
2025-10-05 02:51:13 -0300

    agrego funconalidad al mapper de colecciones

--- Agregador/src/main/java/ar/utn/dssi/Agregador/models/mappers/MapperDeColecciones.java
+++ Agregador/src/main/java/ar/utn/dssi/Agregador/models/mappers/MapperDeColecciones.java
@@ -1,4 +1,16 @@
 package ar.utn.dssi.Agregador.models.mappers;
 
+import ar.utn.dssi.Agregador.models.DTOs.outputDTO.ColeccionOutputDTO;
+import ar.utn.dssi.Agregador.models.entities.Coleccion;
+
 public class MapperDeColecciones {
+  public static ColeccionOutputDTO coleccionOutputDTOFromColeccion(Coleccion coleccion) {
+    var coleccionDto = new ColeccionOutputDTO();
+
+    coleccionDto.setTitulo(coleccion.getTitulo());
+    coleccionDto.setDescripcion(coleccion.getDescripcion());
+    coleccionDto.setHechos(coleccion.getHechos());
+
+    return coleccionDto;
+  }
 }
