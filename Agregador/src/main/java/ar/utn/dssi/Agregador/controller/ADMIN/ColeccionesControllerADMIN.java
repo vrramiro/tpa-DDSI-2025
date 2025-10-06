9d6554e02d6a45bc67e8ef12fc4189f44d7af3dd
vramma_ <ramivera2003@gmail.com>
2025-10-06 01:13:01 -0300

    corrijo endpoints

--- Agregador/src/main/java/ar/utn/dssi/Agregador/controller/ADMIN/ColeccionesControllerADMIN.java
+++ Agregador/src/main/java/ar/utn/dssi/Agregador/controller/ADMIN/ColeccionesControllerADMIN.java
@@ -4,7 +4,6 @@ import ar.utn.dssi.Agregador.models.DTOs.inputDTO.ColeccionInputDTO;
 import ar.utn.dssi.Agregador.models.DTOs.outputDTO.ColeccionOutputDTO;
 import ar.utn.dssi.Agregador.models.DTOs.outputDTO.HechoOutputDTO;
 import ar.utn.dssi.Agregador.services.IColeccionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/colecciones")
 public class ColeccionesControllerADMIN {
-  @Autowired
-  private IColeccionService coleccionService;
+  private final IColeccionService coleccionService;
+
+  public ColeccionesControllerADMIN(IColeccionService coleccionService) {
+    this.coleccionService = coleccionService;
+  }
 
   //OPERACIONES CRUD SOBRE COLECCIONES
-  @PostMapping("/hecho")
+  @PostMapping
   public ResponseEntity<ColeccionOutputDTO> crearColeccion(@RequestBody ColeccionInputDTO coleccionInputDTO){
     ColeccionOutputDTO coleccionOutputDTO = coleccionService.crearColeccion(coleccionInputDTO);
 
@@ -30,15 +32,15 @@ public class ColeccionesControllerADMIN {
     List<ColeccionOutputDTO> colecciones = coleccionService.obtenerColecciones();
 
     if(colecciones.isEmpty()){
-      return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //status 204
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // status 204
     }
 
     return ResponseEntity.ok(colecciones); // status 200
   }
 
-  @GetMapping("/hechos/{handle}")
+  @GetMapping("/{handle}/hechos")
   public ResponseEntity<List<HechoOutputDTO>> obtenerHechosDeColeccion(@PathVariable String handle) {
-    List<HechoOutputDTO> hechosDeColeccion = coleccionService.hechosDeColeccion(handle);
+    List<HechoOutputDTO> hechosDeColeccion = coleccionService.obtenerHechosDeColeccion(handle);
 
     if(hechosDeColeccion.isEmpty()){
       return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // status 204
@@ -47,14 +49,14 @@ public class ColeccionesControllerADMIN {
     return ResponseEntity.ok(hechosDeColeccion); // status 200
   }
 
-  @PutMapping("/coleccion/{handle}")
+  @PutMapping("/{handle}")
   public ResponseEntity<ColeccionOutputDTO> actualizarColeccion(@PathVariable String handle, @RequestBody ColeccionInputDTO coleccionInputDTO){
     ColeccionOutputDTO coleccionOutputDTO = coleccionService.actualizarColeccion(handle, coleccionInputDTO);
 
     return ResponseEntity.status(HttpStatus.ACCEPTED).body(coleccionOutputDTO); // status 201
   }
 
-  @DeleteMapping("/coleccion/{handle}")
+  @DeleteMapping("/{handle}")
   public ResponseEntity<Void> eliminarColeccion(@PathVariable String handle){
     coleccionService.eliminarColeccion(handle);
 
