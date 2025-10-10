0812ba13faf0d0afb4dc19da5c1dcb0a0279fe9f
vramma_ <ramivera2003@gmail.com>
2025-10-10 16:33:41 -0300

    agrego parametro para actualizacion

--- Agregador/src/main/java/ar/utn/dssi/Agregador/models/entities/criteriosDePertenencia/CriterioDePertenenciaFactory.java
+++ Agregador/src/main/java/ar/utn/dssi/Agregador/models/entities/criteriosDePertenencia/CriterioDePertenenciaFactory.java
@@ -8,23 +8,29 @@ import ar.utn.dssi.Agregador.models.entities.criteriosDePertenencia.impl.Criteri
 import java.time.LocalDate;
 
 public class CriterioDePertenenciaFactory {
-    public static CriterioDePertenencia crearCriterio(TipoCriterio tipo, String valorDelCriterio) {
+    //Si llega id null es porque es nuevo
+    //Si llega id con valor es porque ya existia y se esta editando
+    public static CriterioDePertenencia crearCriterio(Long id, TipoCriterio tipo, String valorDelCriterio) {
         switch(tipo) {
             case FECHA_DESDE:
                 CriterioFechaDesde criterioFechaDesde = new CriterioFechaDesde();
                 criterioFechaDesde.setFechaDesde(LocalDate.parse(valorDelCriterio));
+                criterioFechaDesde.setId(id);
                 return criterioFechaDesde;
             case FECHA_HASTA:
                 CriterioFechaHasta criterioFechaHasta = new CriterioFechaHasta();
                 criterioFechaHasta.setFechaHasta(LocalDate.parse(valorDelCriterio));
+                criterioFechaHasta.setId(id);
                 return criterioFechaHasta;
             case CATEGORIA:
                 CriterioPorCategoria criterioCategoria = new CriterioPorCategoria();
                 criterioCategoria.setCategoria(valorDelCriterio);
+                criterioCategoria.setId(id);
                 return criterioCategoria;
             case PROVINCIA:
                 CriterioPorProvincia criterioProvincia = new CriterioPorProvincia();
                 criterioProvincia.setProvincia(valorDelCriterio);
+                criterioProvincia.setId(id);
                 return criterioProvincia;
             default:
                 return null;
