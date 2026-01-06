efe8017cf7132a57647febb606ac31b53fd386fc
SantiFustinoni <164821486+SantiFustinoni@users.noreply.github.com>
2026-01-06 00:23:25 -0300

    Modify main method to set web application type
    Set web application type to SERVLET to resolve conflict.

--- FuenteDinamica/src/main/java/ar/utn/dssi/FuenteDinamica/FuenteDinamicaApplication.java
+++ FuenteDinamica/src/main/java/ar/utn/dssi/FuenteDinamica/FuenteDinamicaApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FuenteDinamicaApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.run(FuenteDinamicaApplication.class, args);
-  }
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(FuenteDinamicaApplication.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET); // Esto arregla el conflicto
+        app.run(args);
+    }
 }
